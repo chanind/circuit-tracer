13e7cbf91920006fd6713d225080421b8d7e2625
David Chanin <chanindav@gmail.com>
2025-09-13 00:08:59 +0100

    trying to add some comparison tests to help debug why things aren't working

---
+++ tests/test_replacement_model.py
@@ -0,0 +1,73 @@
+import pytest
+import torch
+import torch.nn.functional as F
+from transformer_lens import HookedTransformer
+from transformer_lens.model_bridge import TransformerBridge
+from transformers import AutoTokenizer, GPT2LMHeadModel
+
+from circuit_tracer.replacement_model import ReplacementModel
+from circuit_tracer.transcoder.single_layer_transcoder import SingleLayerTranscoder, TranscoderSet
+from tests._comparison.replacement_model import ReplacementModel as LegacyReplacementModel
+
+
+@pytest.fixture
+def gpt2_transcoder_set() -> TranscoderSet:
+    return TranscoderSet(
+        transcoders={
+            i: SingleLayerTranscoder(
+                d_model=768,
+                d_transcoder=128,
+                layer_idx=i,
+                activation_function=F.relu,
+            )
+            for i in range(12)
+        },
+        feature_input_hook="hook_resid_mid",
+        feature_output_hook="hook_mlp_out",
+    )
+
+
+def test_bridge_gpt2_replacement_model_behaves_like_legacy_replacement_model(
+    gpt2_transcoder_set: TranscoderSet,
+):
+    hf_model = GPT2LMHeadModel.from_pretrained("gpt2")
+    hf_tokenizer = AutoTokenizer.from_pretrained("gpt2")
+    legacy_model = LegacyReplacementModel.from_pretrained_and_transcoders(
+        "gpt2", gpt2_transcoder_set
+    )
+    bridge_model = ReplacementModel.from_hf_model(hf_model, hf_tokenizer, gpt2_transcoder_set)
+
+    test_inputs = hf_tokenizer.encode("Hello, world!", return_tensors="pt")
+
+    legacy_logits, legacy_cache = legacy_model.run_with_cache(test_inputs)
+    bridge_logits, bridge_cache = bridge_model.run_with_cache(test_inputs)
+
+    assert torch.allclose(legacy_logits, bridge_logits)  # type: ignore
+
+    for layer in range(12):
+        assert torch.allclose(
+            legacy_cache[f"blocks.{layer}.hook_resid_mid"],
+            bridge_cache[f"blocks.{layer}.hook_resid_mid"],
+            atol=1e-2,
+            rtol=1e-2,
+        )
+
+
+def test_TransformerBridge_gpt2_behaves_like_HookedTransformer_gpt2():
+    """
+    This isn't actually a test of the ReplacementModel, but if this fails, we have no hope.
+    """
+    legacy_gpt2 = HookedTransformer.from_pretrained_no_processing("gpt2")
+    bridge_gpt2: TransformerBridge = TransformerBridge.boot_transformers("gpt2")  # type: ignore
+    bridge_gpt2.enable_compatibility_mode()
+
+    test_inputs = bridge_gpt2.tokenizer.encode("Hello, world!", return_tensors="pt")
+
+    legacy_logits, legacy_cache = legacy_gpt2.run_with_cache(test_inputs)
+    bridge_logits, bridge_cache = bridge_gpt2.run_with_cache(test_inputs)
+
+    assert torch.allclose(legacy_logits, bridge_logits)  # type: ignore
+    for shared_hook in legacy_cache.keys() & bridge_cache.keys():
+        assert torch.allclose(
+            legacy_cache[shared_hook], bridge_cache[shared_hook], atol=1e-2, rtol=1e-2
+        ), f"{shared_hook} is not close"
