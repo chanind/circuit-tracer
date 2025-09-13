056fea3021bb7b38eb2f48ab28ba8041f9f34f64
David Chanin <chanindav@gmail.com>
2025-09-13 11:36:39 +0100

    switching to 3.x branch

--- tests/test_replacement_model.py
+++ tests/test_replacement_model.py
@@ -30,12 +30,14 @@ def gpt2_transcoder_set() -> TranscoderSet:
 def test_bridge_gpt2_replacement_model_behaves_like_legacy_replacement_model(
     gpt2_transcoder_set: TranscoderSet,
 ):
-    hf_model = GPT2LMHeadModel.from_pretrained("gpt2")
+    hf_model = GPT2LMHeadModel.from_pretrained("gpt2", device_map="cpu")
     hf_tokenizer = AutoTokenizer.from_pretrained("gpt2")
     legacy_model = LegacyReplacementModel.from_pretrained_and_transcoders(
-        "gpt2", gpt2_transcoder_set
+        "gpt2", gpt2_transcoder_set, device="cpu"
     )
-    bridge_model = ReplacementModel.from_hf_model(hf_model, hf_tokenizer, gpt2_transcoder_set)
+    bridge_model = ReplacementModel.from_hf_model(
+        hf_model, hf_tokenizer, gpt2_transcoder_set, device=torch.device("cpu")
+    )
 
     test_inputs = hf_tokenizer.encode("Hello, world!", return_tensors="pt")
 
@@ -57,17 +59,23 @@ def test_TransformerBridge_gpt2_behaves_like_HookedTransformer_gpt2():
     """
     This isn't actually a test of the ReplacementModel, but if this fails, we have no hope.
     """
-    legacy_gpt2 = HookedTransformer.from_pretrained_no_processing("gpt2")
-    bridge_gpt2: TransformerBridge = TransformerBridge.boot_transformers("gpt2")  # type: ignore
-    bridge_gpt2.enable_compatibility_mode()
+    legacy_gpt2 = HookedTransformer.from_pretrained_no_processing("gpt2", device_map="cpu")
+    bridge_gpt2: TransformerBridge = TransformerBridge.boot_transformers("gpt2", device="cpu")  # type: ignore
+    bridge_gpt2.enable_compatibility_mode(no_processing=True)
 
     test_inputs = bridge_gpt2.tokenizer.encode("Hello, world!", return_tensors="pt")
 
     legacy_logits, legacy_cache = legacy_gpt2.run_with_cache(test_inputs)
     bridge_logits, bridge_cache = bridge_gpt2.run_with_cache(test_inputs)
 
-    assert torch.allclose(legacy_logits, bridge_logits)  # type: ignore
-    for shared_hook in legacy_cache.keys() & bridge_cache.keys():
+    def assert_hook_close(legacy_cache, bridge_cache, hook):
         assert torch.allclose(
-            legacy_cache[shared_hook], bridge_cache[shared_hook], atol=1e-2, rtol=1e-2
-        ), f"{shared_hook} is not close"
+            legacy_cache[hook],
+            bridge_cache[hook],
+            atol=1e-2,
+            rtol=1e-2,
+        ), f"{hook} is not close"
+
+    assert torch.allclose(legacy_logits, bridge_logits)  # type: ignore
+    for layer in range(12):
+        assert_hook_close(legacy_cache, bridge_cache, f"blocks.{layer}.hook_resid_mid")
